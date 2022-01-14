475cd2fa42247ba5d934d5b742b493d8e1a4c090
Bruno Barbosa <babosa227@gmail.com>
2022-01-13 23:58:53 -0300

    Projeto criado

---
+++ src/main/java/br/com/brunobarbosa/rest/loja/RestLojaApplication.java
@@ -0,0 +1,13 @@
+package br.com.brunobarbosa.rest.loja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestLojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestLojaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/brunobarbosa/rest/loja/RestLojaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brunobarbosa.rest.loja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestLojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
