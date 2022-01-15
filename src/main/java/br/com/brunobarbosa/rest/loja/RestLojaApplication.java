2e476638c57f7949cf3c8705a1e6565ce3f305d2
Bruno Barbosa <babosa227@gmail.com>
2022-01-15 13:13:32 -0300

    Criação da entidade Produto e associação muitos para muitos com a entidade Categoria

--- src/main/java/br/com/brunobarbosa/rest/loja/RestLojaApplication.java
+++ src/main/java/br/com/brunobarbosa/rest/loja/RestLojaApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.brunobarbosa.rest.loja.domain.Categoria;
+import br.com.brunobarbosa.rest.loja.domain.Produto;
 import br.com.brunobarbosa.rest.loja.repositories.CategoriaRepository;
+import br.com.brunobarbosa.rest.loja.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class RestLojaApplication implements CommandLineRunner{
 
 	@Autowired
 	private CategoriaRepository catRepository;
+	
+	@Autowired
+	private ProdutoRepository prodRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(RestLojaApplication.class, args);
@@ -24,9 +29,20 @@ public class RestLojaApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		Produto prod1 = new Produto(null, "Computador", 2000.00);
+		Produto prod2 = new Produto(null, "Impressora", 800.00);
+		Produto prod3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(prod1, prod2, prod3));
+		cat2.getProdutos().addAll(Arrays.asList(prod2));
+		
+		prod1.getCategorias().addAll(Arrays.asList(cat1));
+		prod2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		prod3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		catRepository.saveAll(Arrays.asList(cat1, cat2));
-		
-		
+		prodRepository.saveAll(Arrays.asList(prod1, prod2, prod3));
 	}
 
 }
