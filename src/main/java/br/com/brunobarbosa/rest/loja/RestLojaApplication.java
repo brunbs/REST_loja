0a9766d1542544bda7acd7322e24803865b9a81a
Bruno Barbosa <babosa227@gmail.com>
2022-01-15 12:51:48 -0300

    Criando operação de instanciação de Categorias

--- src/main/java/br/com/brunobarbosa/rest/loja/RestLojaApplication.java
+++ src/main/java/br/com/brunobarbosa/rest/loja/RestLojaApplication.java
@@ -1,13 +1,32 @@
 package br.com.brunobarbosa.rest.loja;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class RestLojaApplication {
+import br.com.brunobarbosa.rest.loja.domain.Categoria;
+import br.com.brunobarbosa.rest.loja.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class RestLojaApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository catRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RestLojaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		catRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		
 	}
 
 }
