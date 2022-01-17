cb0db32e030a7e3cd8d3da6127a3380bfd6e03cd
Bruno Barbosa <babosa227@gmail.com>
2022-01-17 13:40:39 -0300

    Implementação de Paginação com parâmetros opcionais na requisição de Categorias

--- src/main/java/br/com/brunobarbosa/rest/loja/services/CategoriaService.java
+++ src/main/java/br/com/brunobarbosa/rest/loja/services/CategoriaService.java
@@ -6,6 +6,9 @@ import java.util.Optional;
 import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.brunobarbosa.rest.loja.domain.Categoria;
@@ -45,6 +48,11 @@ public class CategoriaService {
 	
 	public List<Categoria> buscarTodos() {
 		return categoriaRepository.findAll();
+	}
+	
+	public Page<Categoria> buscaComPaginacao(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return categoriaRepository.findAll(pageRequest);
 	}
 	
 }
