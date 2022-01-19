6242fd30c47065e30dfda9bc6f3dc4c9521e1ed7
Bruno Barbosa <babosa227@gmail.com>
2022-01-19 10:36:34 -0300

    Inserindo Pedido utilizando as classes derivadas PagamentoComCartao e PagamentoComBoleto

--- src/main/java/br/com/brunobarbosa/rest/loja/resources/PedidoResource.java
+++ src/main/java/br/com/brunobarbosa/rest/loja/resources/PedidoResource.java
@@ -1,11 +1,18 @@
 package br.com.brunobarbosa.rest.loja.resources;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.brunobarbosa.rest.loja.domain.Pedido;
 import br.com.brunobarbosa.rest.loja.services.PedidoService;
@@ -24,5 +31,12 @@ public class PedidoResource {
 		return ResponseEntity.ok(pedidoBuscado);
 	}
 	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido pedido) {
+		pedido = pedidoService.inserir(pedido);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(pedido.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 	
 }
--- src/main/java/br/com/brunobarbosa/rest/loja/services/PedidoService.java
+++ src/main/java/br/com/brunobarbosa/rest/loja/services/PedidoService.java
@@ -1,12 +1,19 @@
 package br.com.brunobarbosa.rest.loja.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.com.brunobarbosa.rest.loja.domain.ItemPedido;
+import br.com.brunobarbosa.rest.loja.domain.PagamentoComBoleto;
 import br.com.brunobarbosa.rest.loja.domain.Pedido;
+import br.com.brunobarbosa.rest.loja.domain.enums.EstadoPagamento;
+import br.com.brunobarbosa.rest.loja.repositories.ItemPedidoRepository;
+import br.com.brunobarbosa.rest.loja.repositories.PagamentoRepository;
 import br.com.brunobarbosa.rest.loja.repositories.PedidoRepository;
 
 @Service
@@ -15,10 +22,43 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository pedidoRepository;
 	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
 	public Pedido buscar(Integer id) {
 		Optional<Pedido> retornoPedido = pedidoRepository.findById(id);
 		return retornoPedido.orElseThrow(() -> new ObjectNotFoundException(
 				"Id: " + id + ", Tipo: " + Pedido.class.getName(), "Objeto Não Encontrado"));
+	}
+	
+	@Transactional
+	public Pedido inserir(Pedido pedido) {
+		pedido.setId(null);
+		pedido.setInstante(new Date());
+		pedido.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		pedido.getPagamento().setPedido(pedido);
+		if (pedido.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) pedido.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, pedido.getInstante());
+		}
+		pedido = pedidoRepository.save(pedido);
+		pagamentoRepository.save(pedido.getPagamento());
+		for(ItemPedido ip : pedido.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.buscar(ip.getProduto().getId()).getPreco());
+			ip.setPedido(pedido);
+		}
+		itemPedidoRepository.saveAll(pedido.getItens());
+		return pedido;
 	}
 	
 }
