05a4a2b37daa47502f46c1761e4ab14ef18b91ef
Bruno Barbosa <babosa227@gmail.com>
2022-01-24 17:05:02 -0300

    Implementando autenticação e geração de token JWT

--- src/main/java/br/com/brunobarbosa/rest/loja/config/SecurityConfig.java
+++ src/main/java/br/com/brunobarbosa/rest/loja/config/SecurityConfig.java
@@ -1,21 +1,37 @@
 package br.com.brunobarbosa.rest.loja.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import br.com.brunobarbosa.rest.loja.security.JWTAuthenticationFilter;
+import br.com.brunobarbosa.rest.loja.security.JWTUtil;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Autowired
+	private Environment env;
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+	
 	private static final String[] PUBLIC_MATCHERS_GET = {
 			"/produtos/**",
 			"/categorias/**",
@@ -27,7 +43,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		http.authorizeRequests()
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.anyRequest().authenticated();
+		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+	
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
 	}
 	
 	@Bean
---
+++ src/main/java/br/com/brunobarbosa/rest/loja/services/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package br.com.brunobarbosa.rest.loja.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import br.com.brunobarbosa.rest.loja.domain.Cliente;
+import br.com.brunobarbosa.rest.loja.repositories.ClienteRepository;
+import br.com.brunobarbosa.rest.loja.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = clienteRepository.findByEmail(email);
+		if (cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	}
+
+}
